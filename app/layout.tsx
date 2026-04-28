cfb7964ae46d052d846cda7018d37a21e1b4d93f
Thomas Crook <icu@MacBook-Pro-de-Thomas-2.local>
2026-04-28 10:51:18 -0400

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -31,7 +32,14 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8374059668336442"
+          crossOrigin="anonymous"
+        />
+        {children}
+      </body>
     </html>
   );
 }
